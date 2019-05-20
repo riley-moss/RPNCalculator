c41e23c7acac01dbd77db7718010f53fe36ddb09
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-05-20 12:56:01 -0500

    Updated test names to be more generic

--- app/src/test/java/com/example/rpncalculator/RPNCalculatorTest.kt
+++ app/src/test/java/com/example/rpncalculator/RPNCalculatorTest.kt
@@ -15,57 +15,57 @@ class RPNCalculatorTest {
     private val calc = RPNCalculator()
 
     @Test
-    fun `given '1' return 1`() {
+    fun `given a number return that number`() {
         assertEquals(1, calc.calculate("1"))
     }
 
     @Test
-    fun `given '2' return 2`() {
+    fun `given a different number return that number`() {
         assertEquals(2, calc.calculate("2"))
     }
 
     @Test
-    fun `given '1 1 +' return 2`() {
+    fun `given 2 numbers and a plus sign return those numbers added together`() {
         assertEquals(2, calc.calculate("1 1 +"))
     }
 
     @Test
-    fun `given '2 1 -' return 1`() {
+    fun `given 2 numbers and a minus sign return the difference of those numbers`() {
         assertEquals(1, calc.calculate("2 1 -"))
     }
 
     @Test
-    fun `given '5 2 *' return 10`() {
+    fun `given 2 numbers and a multiply sign return those numbers multiplied together`() {
         assertEquals(10, calc.calculate("5 2 *"))
     }
 
     @Test
-    fun `given '9 3 forward-slash' return 3`() {
+    fun `given 2 numbers and a division sign return the value of the first divided by the second`() {
         assertEquals(3, calc.calculate("9 3 /"))
     }
 
     @Test
-    fun `given '1 2 + 3 +' return 6`() {
+    fun `given an input with 2 operands return the correct output based on order of operations`() {
         assertEquals(6, calc.calculate("1 2 + 3 +"))
     }
 
     @Test
-    fun `given '3 5 8 * 7 + *' return 141`() {
+    fun `given an input with 3 operands return the correct output based on order of operations`() {
         assertEquals(141, calc.calculate("3 5 8 * 7 + *"))
     }
 
     @Test
-    fun `given '9 SQRT' return 3`() {
+    fun `given a number and SQRT return the square root of that number`() {
         assertEquals(3, calc.calculate("9 SQRT"))
     }
 
     @Test
-    fun `given '5 3 4 2 9 1 MAX' return 9`() {
+    fun `given a list of numbers and MAX return the max value in all those numbers`() {
         assertEquals(9, calc.calculate("5 3 4 2 9 1 MAX"))
     }
 
     @Test
-    fun `given '4 5 MAX 1 2 MAX *' return 10`() {
+    fun `given two MAX lists and a multiply sign return the max of both lists multiplied together`() {
         assertEquals(10, calc.calculate("4 5 MAX 1 2 MAX *"))
     }
 }
