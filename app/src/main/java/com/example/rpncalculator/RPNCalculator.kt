aa1540fdac69bad9037458eae09330e7222debb6
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-06-13 14:14:51 -0500

    Refactored doMath to accept Ints instead of Strings

--- app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
+++ app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
@@ -2,20 +2,20 @@ package com.example.rpncalculator
 
 class RPNCalculator {
     fun calculate(input: String): Int? {
-        if(input.length == 1) {
+        if (input.length == 1) {
             return Integer.parseInt(input)
         }
 
         val splitInput = input.split(" ")
-        return doMath(splitInput[0], splitInput[1], splitInput[2])
+        return doMath(Integer.parseInt(splitInput[0]), Integer.parseInt(splitInput[1]), splitInput[2])
     }
 
-    private fun doMath(firstNum:String, secondNum:String, operator:String): Int {
+    private fun doMath(firstNum: Int, secondNum: Int, operator: String): Int {
         return when (operator) {
-            "+" -> Integer.parseInt(firstNum) + Integer.parseInt(secondNum)
-            "*" -> Integer.parseInt(firstNum) * Integer.parseInt(secondNum)
-            "/" -> Integer.parseInt(firstNum) / Integer.parseInt(secondNum)
-            else -> Integer.parseInt(firstNum) - Integer.parseInt(secondNum)
+            "+" -> firstNum + secondNum
+            "*" -> firstNum * secondNum
+            "/" -> firstNum / secondNum
+            else -> firstNum - secondNum
         }
     }
 
