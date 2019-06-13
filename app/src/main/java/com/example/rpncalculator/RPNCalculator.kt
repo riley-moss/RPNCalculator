d57d56a8beb5a0c5f3532d41b90c68aa930662eb
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-06-13 13:56:05 -0500

    Moved doMath if-else returns to return when, changed parameters to take in the two numbers and a operator

--- app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
+++ app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
@@ -7,17 +7,16 @@ class RPNCalculator {
         }
 
         val splitInput = input.split(" ")
-        return doMath(splitInput)
+        return doMath(splitInput[0], splitInput[1], splitInput[2])
     }
 
-    private fun doMath(splitInput: List<String>): Int {
-        if (splitInput[2] == "+")
-            return Integer.parseInt(splitInput[0]) + Integer.parseInt(splitInput[1])
-        else if (splitInput[2] == "*")
-            return Integer.parseInt(splitInput[0]) * Integer.parseInt(splitInput[1])
-        else if(splitInput[2] == "/")
-            return Integer.parseInt(splitInput[0]) / Integer.parseInt(splitInput[1])
-        return Integer.parseInt(splitInput[0]) - Integer.parseInt(splitInput[1])
+    private fun doMath(firstNum:String, secondNum:String, operator:String): Int {
+        return when (operator) {
+            "+" -> Integer.parseInt(firstNum) + Integer.parseInt(secondNum)
+            "*" -> Integer.parseInt(firstNum) * Integer.parseInt(secondNum)
+            "/" -> Integer.parseInt(firstNum) / Integer.parseInt(secondNum)
+            else -> Integer.parseInt(firstNum) - Integer.parseInt(secondNum)
+        }
     }
 
 }
