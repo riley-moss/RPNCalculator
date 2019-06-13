100c4c24bbfda0592d36b5c5b57d7f22680e08fc
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-06-13 13:48:47 -0500

    Refactored doing math into a separate function

--- app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
+++ app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
@@ -2,14 +2,18 @@ package com.example.rpncalculator
 
 class RPNCalculator {
     fun calculate(input: String): Int? {
-        val splitInput = input.split(" ")
-        if(splitInput.size == 1) {
+        if(input.length == 1) {
             return Integer.parseInt(input)
         }
 
-        if(splitInput[2] == "+")
+        val splitInput = input.split(" ")
+        return doMath(splitInput)
+    }
+
+    private fun doMath(splitInput: List<String>): Int {
+        if (splitInput[2] == "+")
             return Integer.parseInt(splitInput[0]) + Integer.parseInt(splitInput[1])
-        else if(splitInput[2] == "*")
+        else if (splitInput[2] == "*")
             return Integer.parseInt(splitInput[0]) * Integer.parseInt(splitInput[1])
 
         return Integer.parseInt(splitInput[0]) - Integer.parseInt(splitInput[1])
