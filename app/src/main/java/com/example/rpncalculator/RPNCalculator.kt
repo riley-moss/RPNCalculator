0e7e42650adf8867edc3c628c0cc1fe5685f58ef
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-06-13 15:57:57 -0500

    Re-added doMath function to move math operations out of main function

--- app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
+++ app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
@@ -3,41 +3,43 @@ package com.example.rpncalculator
 import java.util.*
 
 class RPNCalculator {
+
+    private val stack = Stack<Int>()
+    private var prevMaxIndex = 0
     fun calculate(input: String): Int {
         if (input.length == 1) {
             return Integer.parseInt(input)
         }
 
         val splitInput = input.split(" ")
-        val stack = Stack<Int>()
-
-        var prevMaxIndex = 0
         for (item in splitInput) {
-            stack.push(
-                when (item) {
-                    "+" -> stack.pop() + stack.pop()
-                    "*" -> stack.pop() * stack.pop()
-                    "/" -> {
-                        val divisor = stack.pop()
-                        stack.pop() / divisor
-                    }
-                    "-" -> -stack.pop() + stack.pop()
-                    "MAX" -> {
-                        var max = stack.pop()
-                        while (stack.size > prevMaxIndex) {
-                            val popped = stack.pop()
-                            if(popped > max) {
-                                max = popped
-                            }
-                        }
-                        prevMaxIndex++
-                        max
-                    }
-                    "SQRT" -> Math.sqrt(stack.pop().toDouble()).toInt()
-                    else -> Integer.parseInt(item)
-                }
-            )
+            stack.push(doMath(item))
         }
         return stack.pop()
+    }
+
+    private fun doMath(item: String): Int {
+        return when (item) {
+            "+" -> stack.pop() + stack.pop()
+            "*" -> stack.pop() * stack.pop()
+            "/" -> {
+                val divisor = stack.pop()
+                stack.pop() / divisor
+            }
+            "-" -> -stack.pop() + stack.pop()
+            "MAX" -> {
+                var max = stack.pop()
+                while (stack.size > prevMaxIndex) {
+                    val popped = stack.pop()
+                    if (popped > max) {
+                        max = popped
+                    }
+                }
+                prevMaxIndex++
+                max
+            }
+            "SQRT" -> Math.sqrt(stack.pop().toDouble()).toInt()
+            else -> Integer.parseInt(item)
+        }
     }
 }
