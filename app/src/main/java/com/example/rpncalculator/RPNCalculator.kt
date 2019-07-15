4c4d0be9f21103868f1f35bba94a05a1b7ca376e
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-07-15 12:11:17 -0500

    Refactored to return any string into integer

--- app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
+++ app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
@@ -2,11 +2,7 @@ package com.example.rpncalculator
 
 class RPNCalculator {
     fun solveRPN(rpnString: String): Int {
-        if (rpnString == "3")
-            return 3
-        else if (rpnString == "2")
-            return 2
-        return 1
+        return Integer.parseInt(rpnString)
     }
 
 }
