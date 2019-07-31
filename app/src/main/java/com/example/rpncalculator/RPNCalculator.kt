39e81a9e4f66b3eb259f5b16276bb09cf7ad3c9b
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-07-31 15:02:58 -0500

    Restart Kata

--- app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
+++ app/src/main/java/com/example/rpncalculator/RPNCalculator.kt
@@ -1,30 +1,5 @@
 package com.example.rpncalculator
 
-import java.util.*
-
 class RPNCalculator {
-    fun solveRPN(rpnString: String): Int {
-        val rpnArray = rpnString.split(" ")
-
-        val stack = Stack<Int>()
-        for(input in rpnArray){
-            if(input in listOf("+","-","*","/")) {
-                stack.push(calculateBasedOnOperand(stack.pop(), stack.pop(), input))
-            } else {
-                stack.push(input.toInt())
-            }
-        }
-
-        return stack.pop()
-    }
-
-    private fun calculateBasedOnOperand(firstNum: Int, secondNum: Int, operand: String): Int {
-        return when (operand) {
-            "+" -> firstNum + secondNum
-            "-" -> secondNum - firstNum
-            "*" -> firstNum * secondNum
-            else -> secondNum / firstNum
-        }
-    }
 
 }
